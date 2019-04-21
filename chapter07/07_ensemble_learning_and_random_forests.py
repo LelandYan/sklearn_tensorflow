7d780b589e937086b9308b1cee524e8ceffd724d
LelandYan <2721599586@qq.com>
2019-04-21 12:54:27 +0800

    ensemble_learning

--- chapter07/07_ensemble_learning_and_random_forests.py
+++ chapter07/07_ensemble_learning_and_random_forests.py
@@ -17,8 +17,8 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_moons
 
-X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+# X, y = make_moons(n_samples=500, noise=0.30, random_state=42)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 # hard voting
 # log_clf = LogisticRegression(solver="lbfgs", random_state=42)
@@ -53,19 +53,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 
-bag_clf = BaggingClassifier(
-    DecisionTreeClassifier(random_state=42), n_estimators=500, max_samples=100, bootstrap=True, random_state=42
-)
-bag_clf.fit(X_train, y_train)
-y_pred = bag_clf.predict(X_test)
-from sklearn.metrics import accuracy_score
-
-print("使用bagging的决策树：",accuracy_score(y_test, y_pred))
-
-tree_clf = DecisionTreeClassifier(random_state=42)
-tree_clf.fit(X_train, y_train)
-y_pred_tree = tree_clf.predict(X_test)
-print("决策树：",accuracy_score(y_test, y_pred_tree))
+# bag_clf = BaggingClassifier(
+#     DecisionTreeClassifier(random_state=42), n_estimators=500, max_samples=100, bootstrap=True, random_state=42
+# )
+# bag_clf.fit(X_train, y_train)
+# y_pred = bag_clf.predict(X_test)
+# from sklearn.metrics import accuracy_score
+#
+# print("使用bagging的决策树：",accuracy_score(y_test, y_pred))
+#
+# tree_clf = DecisionTreeClassifier(random_state=42)
+# tree_clf.fit(X_train, y_train)
+# y_pred_tree = tree_clf.predict(X_test)
+# print("决策树：",accuracy_score(y_test, y_pred_tree))
 
 from matplotlib.colors import ListedColormap
 
@@ -164,19 +164,107 @@ from sklearn.ensemble import AdaBoostClassifier
 # plt.show()
 from sklearn.tree import DecisionTreeRegressor
 
-from sklearn.ensemble import GradientBoostingClassifier,GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
 # gbrt_slow = GradientBoostingRegressor(max_depth=2, n_estimators=200, learning_rate=0.1, random_state=42)
 # gbrt_slow.fit(X, y)
 
 # Gradient Boosting with Early stopping
 from sklearn.metrics import mean_squared_error
-X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
-gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=120, random_state=42)
-gbrt.fit(X_train, y_train)
+# X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=49)
+# gbrt = GradientBoostingRegressor(max_depth=2, n_estimators=120, random_state=42)
+# gbrt.fit(X_train, y_train)
+#
+# errors = [mean_squared_error(y_val, y_pred)
+#           for y_pred in gbrt.staged_predict(X_val)]
+# bst_n_estimators = np.argmin(errors)
+#
+# gbrt_best = GradientBoostingRegressor(max_depth=2,n_estimators=bst_n_estimators, random_state=42)
+# gbrt_best.fit(X_train, y_train)
+#
+# from sklearn.linear_model import LogisticRegression
+# log_clf = LogisticRegression().fit(X_train,y_train)
 
-errors = [mean_squared_error(y_val, y_pred)
-          for y_pred in gbrt.staged_predict(X_val)]
-bst_n_estimators = np.argmin(errors)
+from xgboost import XGBRegressor
 
-gbrt_best = GradientBoostingRegressor(max_depth=2,n_estimators=bst_n_estimators, random_state=42)
-gbrt_best.fit(X_train, y_train)
+# xgb_reg = XGBRegressor(random_state=42)
+# xgb_reg.fit(X_train, y_train)
+# y_pred = xgb_reg.predict(X_val)
+# val_error = mean_squared_error(y_val, y_pred)
+# print("Validation MSE:", val_error)
+# xgb_reg.fit(X_train, y_train,
+#                 eval_set=[(X_val, y_val)], early_stopping_rounds=2)
+# y_pred = xgb_reg.predict(X_val)
+# val_error = mean_squared_error(y_val, y_pred)  # Not shown
+# print("Validation MSE:", val_error)
+
+from sklearn.model_selection import train_test_split
+from scipy.io import loadmat
+
+# 导入数据
+mnist = loadmat("mnist-original.mat")
+X = mnist["data"].T
+y = mnist["label"][0]
+
+X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=10000, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train_val, y_train_val, test_size=10000, random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+
+random_forest_clf = RandomForestClassifier(n_estimators=100, random_state=42)
+extra_trees_clf = ExtraTreesClassifier(n_estimators=100, random_state=42)
+svm_clf = LinearSVC(random_state=42)
+mlp_clf = MLPClassifier(random_state=42)
+
+estimators = [random_forest_clf, extra_trees_clf, svm_clf, mlp_clf]
+for estimator in estimators:
+    print("Training the:", estimator)
+    estimator.fit(X_train, y_train)
+
+validation_res = [estimator.score(X_val, y_val) for estimator in estimators]
+print(validation_res)
+
+from sklearn.ensemble import VotingClassifier
+
+named_estimators = [
+    ("random_forest_clf", random_forest_clf),
+    ("extra_trees_clf", extra_trees_clf),
+    ("svm_clf", svm_clf),
+    ("mlp_clf", mlp_clf)
+]
+voting_clf = VotingClassifier(named_estimators)
+voting_clf.fit(X_train, y_train)
+print("硬投票的结果:", voting_clf.score(X_val, y_val))
+
+# 将投票的分类器中的svm置空,但是依然存在
+voting_clf.set_params(svm_clf=None)
+print(voting_clf.estimators)
+print(voting_clf.estimators_)
+# 删除svm分类器
+del voting_clf.estimators_[2]
+print(voting_clf.score(X_val, y_val))
+# 采用软分类
+voting_clf.voting = "soft"
+print(voting_clf.score(X_val, y_val))
+print(voting_clf.score(X_test, y_test))
+res = [estimator.score(X_test, y_test) for estimator in voting_clf.estimators_]
+print(res)
+
+# Stacking
+X_val_predictions = np.empty((len(X_val), len(estimators)), dtype=np.float32)
+for index, estimator in enumerate(estimators):
+    X_val_predictions[:, index] = estimator.predict(X_val)
+
+rnd_forest_blender = RandomForestClassifier(n_estimators=200, oob_score=True, random_state=42)
+rnd_forest_blender.fit(X_val_predictions, y_val)
+print(rnd_forest_blender.oob_score_)
+
+X_test_predictions = np.empty((len(X_test), len(estimators)), dtype=np.float32)
+
+for index, estimator in enumerate(estimators):
+    X_test_predictions[:, index] = estimator.predict(X_test)
+y_pred = rnd_forest_blender.predict(X_test_predictions)
+from sklearn.metrics import accuracy_score
+
+print(accuracy_score(y_test, y_pred))
